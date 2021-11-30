6b6514edad8f56feb673a6dfa34df2b68a302645
JuanCamilo <juank-963@hotmail.com>
2021-11-30 16:58:53 -0500

    solving typos

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,18 +1,20 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import {users} from '../../models/user.model'
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnChanges {
 
   @Input() user: users = {
     name: "",
     age: 0,
     img: ''
   }
-
+  ngOnChanges(){
+    console.log('constructor','User Name =', this.user.name);
+  }
   constructor() {
     console.log('constructor','User Name =', this.user.name);
 
